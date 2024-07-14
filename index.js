ac23c27f58ca14e321db7fa2b4bfd207450d0ff0
Sama Durani <duranisama@gmail.com>
2024-07-14 15:26:20 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,21 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add(a, b) {return a + b;}
+function subtract(a, b) {return a - b;}
+function multiply(a, b) {return a * b;}
+function divide(a, b) {return a / b;}
+function increment(n) {
+    n++;
+    return n;}
+function decrement(n) {
+    n--;
+    return n;}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
